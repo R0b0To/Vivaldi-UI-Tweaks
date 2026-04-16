59dd927a4c1056483b289e75b16203db9ef72e01
R0b0To <113817188+R0b0To@users.noreply.github.com>
2026-04-17 00:35:26 +0200

    auto update

--- installScript.bat
+++ installScript.bat
@@ -1,40 +1,42 @@
 @echo off
-cd "%~dp0"
+setlocal EnableDelayedExpansion
 
-set "installPath=%LOCALAPPDATA%\Vivaldi\Application"
-set "latestVersionFolder="
+cd /d "%~dp0"
 
+set "installPath=%LOCALAPPDATA%\Vivaldi\Application\"
 echo Searching at: %installPath%
 
-for /d %%d in ("%installPath%\*") do (
-    if exist "%%d\resources\window.html" (
-        set "latestVersionFolder=%%d\resources"
-    )
+for /f "tokens=*" %%a in ('dir /a:-d /b /s "%installPath%"') do (
+    if /i "%%~nxa"=="window.html" set "latestVersionFolder=%%~dpa"
 )
 
-if "%latestVersionFolder%"=="" (
+if "!latestVersionFolder!"=="" (
     echo Could not find Vivaldi installation.
-    pause & exit
+    pause & exit /b
 )
 
-echo Found latest version folder: "%latestVersionFolder%"
+echo Found latest version folder: "!latestVersionFolder!"
 
-if not exist "%latestVersionFolder%\window.bak.html" (
+if not exist "!latestVersionFolder!\window.bak.html" (
     echo Creating backup...
-    copy "%latestVersionFolder%\window.html" "%latestVersionFolder%\window.bak.html"
+    copy "!latestVersionFolder!\window.html" "!latestVersionFolder!\window.bak.html" >nul
 )
 
 echo Building custom.js...
-type *.js > "%latestVersionFolder%\custom.js"
+type "*.js" > "!latestVersionFolder!\custom.js"
 
 echo Patching window.html...
 
+type "!latestVersionFolder!\window.bak.html" ^
+| findstr /v /c:"</body>" ^
+| findstr /v /c:"</html>" ^
+> "!latestVersionFolder!\window.html"
+
 (
-    type "%latestVersionFolder%\window.bak.html" | findstr /v "</body>" | findstr /v "</html>"
     echo     ^<script src="custom.js"^>^</script^>
     echo   ^</body^>
     echo ^</html^>
-) > "%latestVersionFolder%\window.html"
+) >> "!latestVersionFolder!\window.html"
 
 echo Done!
 pause
