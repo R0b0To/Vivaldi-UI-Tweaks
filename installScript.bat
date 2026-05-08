787c850632398a50c60f5cfdbaa315f9c7093857
R0b0To <113817188+R0b0To@users.noreply.github.com>
2026-05-09 00:28:25 +0200

    up

--- installScript.bat
+++ installScript.bat
@@ -1,8 +1,6 @@
 @echo off
 setlocal EnableDelayedExpansion
-
 cd /d "%~dp0"
-
 set "installPath=%LOCALAPPDATA%\Vivaldi\Application\"
 echo Searching at: %installPath%
 
@@ -10,25 +8,34 @@ for /f "tokens=*" %%a in ('dir /a:-d /b /s "%installPath%"') do (
     if /i "%%~nxa"=="window.html" set "latestVersionFolder=%%~dpa"
 )
 
-if "%latestVersionFolder%"=="" (
-	pause & exit
-) else (
-	echo Found latest version folder: "%latestVersionFolder%"
+if "!latestVersionFolder!"=="" (
+    echo Could not find Vivaldi installation.
+    pause & exit /b
+)
+echo Found latest version folder: "!latestVersionFolder!"
+
+if not exist "!latestVersionFolder!window.bak.html" (
+    echo Creating backup...
+    copy "!latestVersionFolder!window.html" "!latestVersionFolder!window.bak.html" >nul
 )
 
-if not exist "%latestVersionFolder%\window.bak.html" (
-	echo Creating a backup of your original window.html file.
-	copy "%latestVersionFolder%\window.html" "%latestVersionFolder%\window.bak.html"
-)
+echo Building custom.js...
+type "*.js" > "!latestVersionFolder!custom.js"
 
-echo copying js files to custom.js
-type *.js > "%latestVersionFolder%\custom.js"
+echo Patching window.html...
+set "pyFile=%temp%\vivaldi_patch.py"
+set "bakFile=!latestVersionFolder!window.bak.html"
+set "outFile=!latestVersionFolder!window.html"
 
-echo patching window.html file
-type "%latestVersionFolder%\window.bak.html" | findstr /v "</body>" | findstr /v "</html>" > "%latestVersionFolder%\window.html"
-echo     ^<script src="custom.js"^>^</script^> >> "%latestVersionFolder%\window.html"
-echo   ^</body^> >> "%latestVersionFolder%\window.html"
-echo ^</html^> >> "%latestVersionFolder%\window.html"
+> "%pyFile%" echo bak = r"!bakFile!"
+>> "%pyFile%" echo out = r"!outFile!"
+>> "%pyFile%" echo src = open(bak, "rb").read()
+>> "%pyFile%" echo inject = b"    <script src='custom.js'></script>" + b"\n"
+>> "%pyFile%" echo patched = src.replace(b"</body>", inject + b"</body>", 1)
+>> "%pyFile%" echo open(out, "wb").write(patched)
+>> "%pyFile%" echo print("Injected!" if inject in patched else "WARNING: </body> not found!")
+
+python "%pyFile%"
 
 echo Done!
 pause
